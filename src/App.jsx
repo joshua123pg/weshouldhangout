dc3ba45718b486cee36bf6b110c9c25864b87f27
Joshua Ong <Josh@macbookpro.mynetworksettings.com>
2025-10-28 00:32:11 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-gradient-to-br from-pink-200 via-orange-100 to-purple-200 text-gray-800">
+      <h1 className="text-4xl font-bold mb-2">we should hangout</h1>
+      <p className="text-lg opacity-80">let’s make it happen.</p>
+    </div>
+  );
+}
